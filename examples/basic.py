77e197438f9e1a888781f8d79b130eae60dc32c5
Prabhav Sharma <prabhavs2004@gmail.com>
2025-12-28 17:55:33 +0530

    Remove test.py and update model in basic.py and test/test.py to use 'smollm2:latest'; add embedding functionality in basic.py.

--- examples/basic.py
+++ examples/basic.py
@@ -6,8 +6,14 @@ print("Available models:", list_models())
 client = OllamaClient()
 
 out = client.generate(
-    model="llama3.1:8b",
+    model="smollm2:latest",
     prompt="Explain Ollama in one line, answer me in json format with only one key reply and your response must be its value nothing else and it must be in {} to make sure json format"
 )
 
+embeddings = client.get_embeddings(
+    model="granite-embedding:30m",
+    prompt="This is the text I want to embed"
+)
+
 print(out)
+print(embeddings)
--- test/test.py
+++ test/test.py
@@ -3,7 +3,7 @@ from ollama_runner import OllamaClient
 client=OllamaClient()
 
 response=client.generate(
-    model="llama3.1:8b",
+    model="smollm2:latest",
     prompt="Explain Ollama in one line, answer me in json format with only one key reply and your response must be its value nothing else and it must be in {} to make sure json format"
 )
 
